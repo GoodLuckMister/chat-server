b3580235ab59ef798fe1c2b5ba81bf22722b1c16
GodLuckMister <paet14@gmail.com>
2021-09-06 16:46:42 +0300

    add websocket server

---
+++ app.js
@@ -0,0 +1,43 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+})
+
+app.use('*', (req, res) => {
+    res.send('Websocket server')
+})
+
+let users = {}
+
+io.on('connection', (client) => {
+    users[client.id] = 'Anonim'
+    broadcast('users', users)
+
+    client.on('change:name', (name) => {
+        users[client.id] = name
+        broadcast('users', users)
+    })
+    client.on('message', (message) => {
+        //Change , sanitize ect.
+        broadcast('message', message)
+    })
+    client.on('disconnect', () => {
+        delete users[client.id]
+        broadcast('users', users)
+    })
+
+    function broadcast(event, data) {
+        client.emit(event, data)
+        client.broadcast.emit(event, data)
+    }
+
+})
+
+const PORT = process.env.PORT || 5000
+
+http.listen(PORT, () => {
+    console.log(`We are started on port ${PORT}`)
+})
